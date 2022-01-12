d4f964b1e58235fb99a1433e345c1eb5720a0a7f
gmatheus10 <gabrielmatheus13@gmail.com>
2022-01-12 13:12:52 -0300

    fixed! all working now

--- Scripts/Configurations/PathfindingConfig.cs
+++ Scripts/Configurations/PathfindingConfig.cs
@@ -3,64 +3,64 @@ using System.Collections.Generic;
 
 public class PathfindingConfig : Node2D
 {
-  [Export]
-  public int MovementSpeed;
-  Pathfinding pathFinding;
-  HierarchicalPathfinding hierarchical;
-  Movement movement;
-  List<Junction> hierarchicalPath;
-  List<Cell> pathCells = new List<Cell>();
-  public delegate void SendAStar(List<Cell> AStar);
-  public event SendAStar OnAStarFound;
+    [Export]
+    public int MovementSpeed;
 
-  private int pathIndex;
-  public override void _Ready()
-  {
-    hierarchical = GetNode<HierarchicalPathfinding>("HierarchicalPathfinding");
-    hierarchical.OnHierarchyComplete += GetPath;
-    movement = GetNode<Movement>("Movement");
-    movement.OnPathReached += UpdatePath;
-  }
-  private void UpdatePath()
-  {
-    pathIndex++;
-    if (pathIndex + 1 < hierarchicalPath.Count)
+    HierarchicalPathfinding hierarchical;
+    Movement movement;
+    List<Junction> hierarchicalPath;
+
+    public delegate void SendAStar(List<Cell> AStar);
+    public event SendAStar OnAStarFound;
+
+    private int pathIndex;
+    public override void _Ready()
     {
-
-      FindPath(hierarchicalPath);
+        hierarchical = GetNode<HierarchicalPathfinding>("HierarchicalPathfinding");
+        hierarchical.OnHierarchyComplete += GetPath;
+        movement = GetNode<Movement>("Movement");
+        movement.OnPathReached += UpdatePath;
     }
-  }
-
-  private void GetPath(List<Junction> hierarchical)
-  {
-    this.hierarchicalPath = hierarchical;
-    Reset();
-    FindPath(hierarchicalPath);
-  }
-  private void Reset()
-  {
-    pathCells.Clear();
-    pathIndex = 0;
-  }
-  private void FindPath(List<Junction> currentHierarchical)
-  {
-    Junction current = currentHierarchical[pathIndex];
-    Junction next = currentHierarchical[pathIndex + 1];
-
-    if (current.cluster == next.cluster)
+    private void UpdatePath()
     {
-      pathFinding = new Pathfinding(current.cluster.ClusterGrid);
+        pathIndex++;
+        if (pathIndex + 1 < hierarchicalPath.Count)
+        {
 
-      List<Cell> path = pathFinding.FindPath(current.WorldPosition, next.WorldPosition, true);
-      pathCells = path;
+            FindPath(hierarchicalPath);
+        }
     }
-    else
+
+    private void GetPath(List<Junction> hierarchical)
     {
-
-      List<Cell> diffGrid = new List<Cell>() { current.GetCell(), next.GetCell() };
-      pathCells = diffGrid;
+        this.hierarchicalPath = hierarchical;
+        Reset();
+        FindPath(hierarchicalPath);
     }
-    OnAStarFound?.Invoke(pathCells);
+    private void Reset()
+    {
+        pathIndex = 0;
+    }
+    private void FindPath(List<Junction> currentHierarchical)
+    {
+        Junction current = currentHierarchical[pathIndex];
+        Junction next = currentHierarchical[pathIndex + 1];
+        List<Cell> pathCells = new List<Cell>();
+        TimeSpent t = new TimeSpent();
+        if (current.cluster == next.cluster)
+        {
+            Pathfinding pathFinding = new Pathfinding(current.cluster.ClusterGrid);
 
-  }
+            List<Cell> path = pathFinding.FindPath(current.WorldPosition, next.WorldPosition, true);
+            pathCells = path;
+        }
+        else
+        {
+
+            List<Cell> diffGrid = new List<Cell>() { current.GetCell(), next.GetCell() };
+            pathCells = diffGrid;
+        }
+        t.Measure("PathfindingConfig");
+        OnAStarFound?.Invoke(pathCells);
+    }
 }
--- Scripts/Pathfinding/Pathfinding.cs
+++ Scripts/Pathfinding/Pathfinding.cs
@@ -2,222 +2,228 @@ using System.Collections.Generic;
 using Godot;
 public class Pathfinding
 {
-  private Grid<Cell> grid;
+    private Grid<Cell> grid;
 
 
-  public Pathfinding(Grid<Cell> grid)
-  {
-    this.grid = grid;
-  }
-  ///  ////////////////////////////////////////////////////////////////////////
-  public Grid<Cell> GetGrid()
-  {
-    return grid;
-  }
-  public List<Cell> FindPath(Vector2 startWorldPosition, Vector2 endWorldPosition, bool WorldPosition)
-  {
-    if (WorldPosition)
+    public Pathfinding(Grid<Cell> grid)
     {
-      Vector2 start = grid.GetGridPosition(startWorldPosition);
-      Vector2 end = grid.GetGridPosition(endWorldPosition);
-      return FindPath(start, end);
+        this.grid = grid;
+    }
+    ///  ////////////////////////////////////////////////////////////////////////
+    public Grid<Cell> GetGrid()
+    {
+        return grid;
+    }
+    public List<Cell> FindPath(Vector2 startWorldPosition, Vector2 endWorldPosition, bool WorldPosition)
+    {
+        if (WorldPosition)
+        {
+            Vector2 start = grid.GetGridPosition(startWorldPosition);
+            Vector2 end = grid.GetGridPosition(endWorldPosition);
+            return FindPath(start, end);
+        }
+
+        return FindPath(startWorldPosition, endWorldPosition);
+
     }
 
-    return FindPath(startWorldPosition, endWorldPosition);
-
-  }
-
-  public List<Cell> FindPath(Vector2 start, Vector2 end)
-  {
-    Cell startCell = grid.GetGridObject(start, true);
-    Cell endCell = grid.GetGridObject(end, true);
-
-    SortedList<float, Cell> openList = new SortedList<float, Cell>();
-    HashSet<Cell> closedList = new HashSet<Cell>();
-
-    openList.Add(0, startCell);
-
-    ScanGridAndSetDefault();
-
-    startCell.gCost = 0;
-    startCell.hCost = Utils.ManhatamDistance(startCell, endCell);
-    startCell.SetFCost();
-    // //
-    while (openList.Count > 0)
+    public List<Cell> FindPath(Vector2 start, Vector2 end)
     {
-      Cell currentCell = openList.Values[0];
-      openList.RemoveAt(0);
-      closedList.Add(currentCell);
-      foreach (Cell neighbour in GetNeighboursList(currentCell))
-      {
-        if (neighbour.Equals(endCell))
-        {
-          neighbour.SetParent(currentCell);
-          return CalculatePath(neighbour);
-        }
+        Cell startCell = grid.GetGridObject(start, true);
+        Cell endCell = grid.GetGridObject(end, true);
 
-        if (closedList.Contains(neighbour))
-        {
-          continue;
-        }
-        int tentativeGCost = currentCell.gCost + Utils.ManhatamDistance(currentCell, neighbour);
+        SortedList<int, Cell> openList = new SortedList<int, Cell>();
+        HashSet<Cell> closedList = new HashSet<Cell>();
 
-        if (tentativeGCost < neighbour.gCost)
-        {
-          SetNeighbourCellPathValues(neighbour, tentativeGCost);
-          IncludeNeighbourOnOpenList(neighbour);
-        }
-      }
-      void SetNeighbourCellPathValues(Cell neighbour, int tentativeGCost)
-      {
-        neighbour.SetParent(currentCell);
-        neighbour.gCost = tentativeGCost;
-        neighbour.hCost = Utils.ManhatamDistance(neighbour, endCell);
-        neighbour.SetFCost();
-      }
-      void IncludeNeighbourOnOpenList(Cell neighbour)
-      {
-        if (neighbour.GridPosition == new Vector2(30, 18))
-        {
+        openList.Add(0, startCell);
 
-        }
-        if (neighbour.GridPosition == new Vector2(30, 19))
-        {
+        ScanGridAndSetDefault();
 
-        }
-        if (!openList.ContainsKey(neighbour.FCost))
+        startCell.gCost = 0;
+        startCell.hCost = Utils.ManhatamDistance(startCell, endCell);
+        startCell.SetFCost();
+        // //
+        while (openList.Count > 0)
         {
-          openList.Add(neighbour.FCost, neighbour);
+            Cell currentCell = openList.Values[0];
+            openList.RemoveAt(0);
+            closedList.Add(currentCell);
+            foreach (Cell neighbour in GetNeighboursList(currentCell))
+            {
+                if (neighbour.Equals(endCell))
+                {
+                    neighbour.SetParent(currentCell);
+                    return CalculatePath(neighbour);
+                }
+
+                if (closedList.Contains(neighbour))
+                {
+                    continue;
+                }
+                int tentativeGCost = currentCell.gCost + Utils.ManhatamDistance(currentCell, neighbour);
+
+                if (tentativeGCost < neighbour.gCost)
+                {
+                    SetNeighbourCellPathValues(neighbour, tentativeGCost);
+                    IncludeNeighbourOnOpenList(neighbour);
+                }
+            }
+            void SetNeighbourCellPathValues(Cell neighbour, int tentativeGCost)
+            {
+                neighbour.SetParent(currentCell);
+                neighbour.gCost = tentativeGCost;
+                neighbour.hCost = Utils.ManhatamDistance(neighbour, endCell);
+                neighbour.SetFCost();
+            }
+            void IncludeNeighbourOnOpenList(Cell neighbour)
+            {
+                if (neighbour.GridPosition == new Vector2(30, 18))
+                {
+
+                }
+                if (neighbour.GridPosition == new Vector2(30, 19))
+                {
+
+                }
+                if (!openList.ContainsKey(neighbour.FCost))
+                {
+                    openList.Add(neighbour.FCost, neighbour);
+                }
+                else
+                {
+                    Cell insider = openList[neighbour.FCost];
+                    if (insider.gCost > neighbour.gCost)
+                    {
+                        //if the gCost is higher, it means that the hCost is lower
+                        openList.Remove(insider.FCost);
+                        openList.Add(neighbour.FCost, neighbour);
+                        insider.SetDefault();
+                        closedList.Add(insider);
+                    }
+                    /* else
+                    {
+                      neighbour.SetParent(null);
+                      closedList.Add(insider);
+                    } */
+                }
+            }
         }
-        else
+        // out of Cell on the open list
+        GD.Print($"Couldn't find the path between cells {startCell} and {endCell}");
+
+
+        return null;
+        void ScanGridAndSetDefault()
         {
-          Cell insider = openList[neighbour.FCost];
-          if (insider.gCost > neighbour.gCost)
-          {
-            //if the gCost is higher, it means that the hCost is lower
-            openList.Remove(neighbour.FCost);
-            openList.Add(neighbour.FCost, neighbour);
-            // insider.SetParent(null);
-          }
-          // else
-          // {
-          //   neighbour.SetParent(null);
-          // }
+            /* GD.Print(grid.GridArray.GetLength(0));
+            GD.Print(grid.GridArray.GetLength(1));
+            GD.Print(grid.OriginPosition); */
+
+            for (int x = 0; x < grid.GridArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GridArray.GetLength(1); y++)
+                {
+                    grid.GridArray[x, y].SetDefault();
+                }
+            }
         }
-      }
+        List<Cell> CalculatePath(Cell goal)
+        {
+            List<Cell> path = new List<Cell>();
+            path.Add(goal);
+            Cell queue = goal;
+            while (queue.Parent != null)
+            {
+                path.Add((Cell)queue.Parent);
+                queue = (Cell)queue.Parent;
+            }
+            path.Reverse();
+            return path;
+        }
     }
-    // out of Cell on the open list
-    GD.Print($"Couldn't find the path between cells {startCell} and {endCell}");
 
-
-    return null;
-    void ScanGridAndSetDefault()
+    private List<Cell> GetNeighboursList(Cell currentCell)
     {
-      for (int x = 0; x < (int)grid.GridSize.x - 1; x++)
-      {
-        for (int y = 0; y < (int)grid.GridSize.y - 1; y++)
+        float centerX = currentCell.WorldPosition.x;
+        float centerY = currentCell.WorldPosition.y;
+
+        float leftPosX = currentCell.WorldPosition.x - grid.CellSize;
+        float rightPosX = currentCell.WorldPosition.x + grid.CellSize;
+        float upPosY = currentCell.WorldPosition.y + grid.CellSize;
+        float downPosY = currentCell.WorldPosition.y - grid.CellSize;
+
+        List<Cell> neighbours = new List<Cell>();
+
+        try
         {
-          grid.GridArray[x, y].SetDefault();
+            if (leftPosX >= grid.OriginPosition.x)
+            {
+                Cell leftCell = grid.GetGridObject(new Vector2(leftPosX, centerY));
+                AddNonWall(leftCell);
+
+                if (downPosY >= grid.OriginPosition.y)
+                {
+                    Cell downLeftCell = grid.GetGridObject(new Vector2(leftPosX, downPosY));
+                    AddNonWall(downLeftCell);
+                }
+
+                if (upPosY < grid.FinalPosition.y)
+                {
+                    Cell upLeftCell = grid.GetGridObject(new Vector2(leftPosX, upPosY));
+                    AddNonWall(upLeftCell);
+                }
+
+            }
+            if (rightPosX < grid.FinalPosition.x)
+            {
+                Cell rightCell = grid.GetGridObject(new Vector2(rightPosX, centerY));
+                AddNonWall(rightCell);
+
+                if (downPosY >= grid.OriginPosition.y)
+                {
+                    Cell rightDownCell = grid.GetGridObject(new Vector2(rightPosX, downPosY));
+                    AddNonWall(rightDownCell);
+                }
+
+                if (upPosY < grid.FinalPosition.y)
+                {
+                    Cell rightUpCell = grid.GetGridObject(new Vector2(rightPosX, upPosY));
+                    AddNonWall(rightUpCell);
+                }
+
+            }
+            if (downPosY >= grid.OriginPosition.y)
+            {
+                Cell downCell = grid.GetGridObject(new Vector2(centerX, downPosY));
+                AddNonWall(downCell);
+            }
+            if (upPosY < grid.FinalPosition.y)
+            {
+                Cell upCell = grid.GetGridObject(new Vector2(centerX, upPosY));
+                AddNonWall(upCell);
+            }
+
         }
-      }
+        catch (System.Exception)
+        {
+            throw;
+        }
+        return neighbours;
+
+        void AddNonWall(Cell cell)
+        {
+            try
+            {
+                if (!cell.IsWall)
+                {
+                    neighbours.Add(cell);
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
     }
-    List<Cell> CalculatePath(Cell goal)
-    {
-      List<Cell> path = new List<Cell>();
-      path.Add(goal);
-      Cell queue = goal;
-      while (queue.Parent != null)
-      {
-        path.Add((Cell)queue.Parent);
-        queue = (Cell)queue.Parent;
-      }
-      path.Reverse();
-      return path;
-    }
-  }
-
-  private List<Cell> GetNeighboursList(Cell currentCell)
-  {
-    float centerX = currentCell.WorldPosition.x;
-    float centerY = currentCell.WorldPosition.y;
-
-    float leftPosX = currentCell.WorldPosition.x - grid.CellSize;
-    float rightPosX = currentCell.WorldPosition.x + grid.CellSize;
-    float upPosY = currentCell.WorldPosition.y + grid.CellSize;
-    float downPosY = currentCell.WorldPosition.y - grid.CellSize;
-
-    List<Cell> neighbours = new List<Cell>();
-
-    try
-    {
-      if (leftPosX >= grid.OriginPosition.x)
-      {
-        Cell leftCell = grid.GetGridObject(new Vector2(leftPosX, centerY));
-        AddNonWall(leftCell);
-
-        if (downPosY >= grid.OriginPosition.y)
-        {
-          Cell downLeftCell = grid.GetGridObject(new Vector2(leftPosX, downPosY));
-          AddNonWall(downLeftCell);
-        }
-
-        if (upPosY < grid.FinalPosition.y)
-        {
-          Cell upLeftCell = grid.GetGridObject(new Vector2(leftPosX, upPosY));
-          AddNonWall(upLeftCell);
-        }
-
-      }
-      if (rightPosX < grid.FinalPosition.x)
-      {
-        Cell rightCell = grid.GetGridObject(new Vector2(rightPosX, centerY));
-        AddNonWall(rightCell);
-
-        if (downPosY >= grid.OriginPosition.y)
-        {
-          Cell rightDownCell = grid.GetGridObject(new Vector2(rightPosX, downPosY));
-          AddNonWall(rightDownCell);
-        }
-
-        if (upPosY < grid.FinalPosition.y)
-        {
-          Cell rightUpCell = grid.GetGridObject(new Vector2(rightPosX, upPosY));
-          AddNonWall(rightUpCell);
-        }
-
-      }
-      if (downPosY >= grid.OriginPosition.y)
-      {
-        Cell downCell = grid.GetGridObject(new Vector2(centerX, downPosY));
-        AddNonWall(downCell);
-      }
-      if (upPosY < grid.FinalPosition.y)
-      {
-        Cell upCell = grid.GetGridObject(new Vector2(centerX, upPosY));
-        AddNonWall(upCell);
-      }
-
-    }
-    catch (System.Exception)
-    {
-      throw;
-    }
-    return neighbours;
-
-    void AddNonWall(Cell cell)
-    {
-      try
-      {
-        if (!cell.IsWall)
-        {
-          neighbours.Add(cell);
-        }
-      }
-      catch (System.Exception)
-      {
-
-        throw;
-      }
-    }
-  }
 }
